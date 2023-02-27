11d8adecd8c1dd7591804904d079a4781f8299d8
vcpersike <vcpersike@gmail.com>
2023-02-27 13:08:51 -0300

    config de atualização

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 
   app.useGlobalPipes(
     new ValidationPipe({
